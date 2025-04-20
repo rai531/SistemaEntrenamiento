597b5d79f793b56e2cfd28140a276b3adfa60fc7
practica1 <raimundo.monsalve531@gmail.com>
2025-04-20 04:33:03 -0400

    se realiza header con menu y botones login y register.Se llama a vista home y se crean secciones

--- FrontendSistemaEntrenamiento/src/app/app.routes.ts
+++ FrontendSistemaEntrenamiento/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
 
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'Login', component: LoginComponent},
+    { path: 'Register', component:RegisterComponent}
 ];
--- FrontendSistemaEntrenamiento/src/app/home/home.component.ts
+++ FrontendSistemaEntrenamiento/src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../components/header/header.component';
+import { LoginComponent } from '../components/auth/login/login.component';
+import { RegisterComponent } from '../components/auth/register/register.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,LoginComponent,RegisterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
